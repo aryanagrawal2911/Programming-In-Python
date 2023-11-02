eff09867976ec81a28b7969bd29f8049a0703a9b
Aryan Agrawal <147967879+aryanagrawal2911@users.noreply.github.com>
2023-11-02 21:26:55 +0530

    Add files via upload

---
+++ Practice Codes/String_Palindrome.py
@@ -0,0 +1,13 @@
+# Check if a string is palindrome
+
+# With appropriately filtering out the special characters and keeping only alphanumeric values
+import re
+
+s = input()     # Take input from blank terminal
+s = re.sub(r'[^a-zA-Z0-9]', '', s)      # Filtering to keep only alphanumeric characters
+print(s == s[::-1])
+
+
+# Without any kind of filter
+s = input()
+print(s == s[::-1])
