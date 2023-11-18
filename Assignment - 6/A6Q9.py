2268d9205e90bba9955f30fd5eea611e04ba3017
Aryan Agrawal <147967879+aryanagrawal2911@users.noreply.github.com>
2023-11-18 17:48:44 +0530

    Updated A6Q9.py

--- Assignment - 6/A6Q9.py
+++ Assignment - 6/A6Q9.py
@@ -1,8 +1,48 @@
+string = input()
+
 def reverse(string):
     return string[::-1]
+
 
 def reverse_word_order(string):
     return ' '.join(string.split()[::-1])
 
-string = input()
-print(f"{reverse(string)}\n{reverse_word_order(string)}")
+
+def palindrome(string):
+    return string == string[::-1]
+
+
+def  delete(string, index):
+    return string[:index] + string[index + 1:]
+
+
+def count(string):
+
+    string = string.lower()
+    v, co = 0, 0
+    vow = "aeiou"
+
+    for ch in string:
+        if ch in vow:
+            v += 1
+        else:
+            co += 1
+
+    return f"Vowels : {v} and Consonants : {co}"
+
+
+def length(string):
+    co = 0
+    for i in string:
+        co += 1
+    return co
+
+
+def removeDuplicates(string : str):
+    sset = set(string)
+    string = ""
+    for ch in sset:
+        string += ch
+    return string
+
+
