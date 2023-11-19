f3c8bc9133c84ac520f8e6d795d90a02bfafaa84
Aryan Agrawal <147967879+aryanagrawal2911@users.noreply.github.com>
2023-11-19 12:41:03 +0530

    Updated A6Q9.py

--- Assignment - 6/A6Q9.py
+++ Assignment - 6/A6Q9.py
@@ -1,22 +1,22 @@
 string = input()
 
-def reverse(string):
+def reverse(string : str) -> str:
     return string[::-1]
 
 
-def reverse_word_order(string):
+def reverse_word_order(string : str) -> str:
     return ' '.join(string.split()[::-1])
 
 
-def palindrome(string):
+def palindrome(string : str) -> bool:
     return string == string[::-1]
 
 
-def  delete(string, index):
+def delete(string : str, index : int) -> str:
     return string[:index] + string[index + 1:]
 
 
-def count(string):
+def count(string : str) -> str:
 
     string = string.lower()
     v, co = 0, 0
@@ -31,18 +31,50 @@ def count(string):
     return f"Vowels : {v} and Consonants : {co}"
 
 
-def length(string):
+def length(string : str) -> int:
+
     co = 0
+
     for i in string:
         co += 1
     return co
 
 
-def removeDuplicates(string : str):
+def removeDuplicates(string : str) -> str:
+
     sset = set(string)
     string = ""
+
     for ch in sset:
         string += ch
     return string
 
 
+def symmetric(string : str) -> bool:
+
+    mid = len(string) // 2
+    return string[:mid] == string[mid:]
+
+
+def maxfrequency(string : str) -> chr:
+
+    dt = {}
+
+    for ch in string:
+        dt[ch] = dt.get(ch, 0) + 1
+
+    return max(dt, key = dt.get)
+
+
+def  countfrequency(string : str) -> dict:
+
+    dt = {}
+
+    for ch in string:
+        dt[ch] = dt.get(ch, 0) + 1
+
+    return dt
+
+
+def atleastone(string : str) -> bool:
+    return string.isalnum()
